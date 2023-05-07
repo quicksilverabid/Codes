5caadc4ac163a9fe41d89df2d66e644240b84af0
Abid Hasan <abidhasankhan0@gmail.com>
2023-05-07 14:05:11 +0600

    BeeCrowd

---
+++ bee/2766.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char string[40];
+    for (int i=1;i<=10;i++){ 
+    cin>>string;
+    
+    if(i==3){
+        cout<<string<<endl;
+    }
+    else if (i==7){
+        cout<<string<<endl;
+    }
+    else if (i==9){
+        cout<<string<<endl;
+    }
+    }
+    return 0;
+}
