4b5c169de89b9855097e0352cf21ed4a4518cdfe
Abid Hasan <abidhasankhan0@gmail.com>
2023-05-15 11:47:33 +0600

    CodeMarshal

---
+++ cm/powoftwo.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void fastio();
+int main(){
+    fastio();
+    int tc,n;
+    cin>>tc;
+    while(tc--){
+        cin>>n;
+        cout<<1;
+        for(int i=0;i<n;i++){  
+            cout<<0;
+    }
+    cout<<"\n";
+    }
+
+    
+
+}
+void fastio(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
