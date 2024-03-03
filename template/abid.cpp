0da5510a845916526a0720c16decb2acf96c8129
Abid Hasan <abidhasankhan0@gmail.com>
2024-03-03 10:46:01 +0600

    template

---
+++ template/abid.cpp
@@ -0,0 +1,35 @@
+/*
+Author: Abid Hasan
+Created:  03-March-2024  10:44:06
+*/ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define __         ios_base::sync_with_stdio(0); cin.tie(0) ; cout.tie(0);
+#define ll         long long
+#define int        ll
+#define endl       "\n"
+#define w(x)       int x; cin>>x; while(x--)
+#define f(i,x)     for(int i=0;i<x;i++)
+#define pb         push_back
+#define pob        pop_back
+#define mp         make_pair
+#define deb(x)     cout<<#x<<" = "<<x<<endl;
+#define all(x)     x.begin(),x.end()
+#define sz(x)      (int) x.size()
+#define fi         first
+#define se         second
+ 
+//void IO(){
+//    #ifndef ONLINE_JUDGE
+//    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+//    #endif
+//}
+ 
+ 
+signed main(){
+    __
+    //IO();
+    
+}
