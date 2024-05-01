18e906c8f62725d970356dbec1724debcc5d616a
Abid Hasan <112768218+quicksilverabid@users.noreply.github.com>
2024-05-01 14:44:56 +0600

    CSES

--- CSES/Counting Rooms.cpp
+++ CSES/Counting Rooms.cpp
@@ -6,8 +6,7 @@ Created:  01-May-2024  13:20:03
 #include<bits/stdc++.h>
 using namespace std;
 #define __         ios_base::sync_with_stdio(0); cin.tie(0) ; cout.tie(0);
-#define ll         long long
-#define int        ll
+#define int        long long
 #define endl       "\n"
 #define f(i,x)     for(int i=0;i<x;++i)
 #define pb         push_back
