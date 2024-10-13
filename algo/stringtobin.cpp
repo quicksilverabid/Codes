f5493cf90a93383a8fa55d6ea1ff5b11cec2ee9d
Abid Hasan <112768218+quicksilverabid@users.noreply.github.com>
2024-10-14 02:15:02 +0600

    Create stringtobin.cpp

---
+++ algo/stringtobin.cpp
@@ -0,0 +1,26 @@
+/*
+Author: Abid Hasan
+Created:  23-May-2024  23:38:32
+*/ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define __         ios_base::sync_with_stdio(0); cin.tie(0) ; cout.tie(0);
+#define ll         long long
+#define endl       "\n"
+ 
+void solve(){
+    string s;
+    cin>>s;
+    int ans=stoull(s,0,2);
+    cout<<ans<<endl;
+}
+ 
+signed main(){
+    __
+    int tc=1;
+    //cin>>tc;
+    while(tc--){
+     solve();
+    }
+}
