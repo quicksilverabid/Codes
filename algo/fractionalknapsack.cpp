bde49bb6bda115f4442a4ce84f85fbef0d49ed84
Abid Hasan <112768218+quicksilverabid@users.noreply.github.com>
2024-10-14 02:07:25 +0600

    Update fractionalknapsack.cpp

--- algo/fractionalknapsack.cpp
+++ algo/fractionalknapsack.cpp
@@ -38,7 +38,7 @@ void solve(){
     int w;
     cin>>w;
     vector<pair<int,int>>vp(n);
-    for(int i=0;i<vp.size;++i){
+    for(int i=0;i<vp.size();++i){
         cin>>vp[i].first>>vp[i].second;
     }
     cout<<fraction(w,vp,n)<<endl;
