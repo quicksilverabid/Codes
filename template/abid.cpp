c884dab2f66d708ba6dcecc3d5376c154e2579d0
Abid Hasan <112768218+quicksilverabid@users.noreply.github.com>
2025-09-15 09:56:15 +0600

    Update abid.cpp

--- template/abid.cpp
+++ template/abid.cpp
@@ -3,6 +3,13 @@ using namespace std;
 #define __         ios_base::sync_with_stdio(0); cin.tie(0) ; cout.tie(0);
 #define ll         long long
 #define endl       "\n"
+/**
+*Description: *x.find\_by\_order(k) : iterator to the k-th index\\
+               x.order\_of\_key(k) : number of items smaller than k
+ */
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template <typename T> using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
  
 //void IO(){
 //    #ifndef ONLINE_JUDGE
