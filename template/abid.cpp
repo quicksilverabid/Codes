e2cc97ed97f1a86221e626e85b893549d7bdf20c
Abid Hasan <112768218+quicksilverabid@users.noreply.github.com>
2025-09-15 09:51:43 +0600

    abid.cpp

--- template/abid.cpp
+++ template/abid.cpp
@@ -2,18 +2,7 @@
 using namespace std;
 #define __         ios_base::sync_with_stdio(0); cin.tie(0) ; cout.tie(0);
 #define ll         long long
-#define int        ll
 #define endl       "\n"
-#define f(i,x)     for(int i=0;i<x;++i)
-#define pb         push_back
-#define pob        pop_back
-#define mp         make_pair
-#define deb(x)     cout<<#x<<" = "<<x<<endl;
-#define all(x)     x.begin(),x.end()
-#define allr(x)    x.rbegin(),x.rend()
-#define sz(x)      (int) x.size()
-#define fi         first
-#define se         second
  
 //void IO(){
 //    #ifndef ONLINE_JUDGE
@@ -26,7 +15,7 @@ void solve(){
 	
 }
  
-signed main(){
+int main(){
 	__
 	//IO();
 	int tc=1;
